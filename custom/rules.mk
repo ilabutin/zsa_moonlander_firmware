4704a28622ab1dca3ccd20fb973b05a9a83359ee
Igor Labutin <igor@labutin.info>
2023-11-04 23:41:05 +0300

    Simplest custom keymap added

---
+++ custom/rules.mk
@@ -0,0 +1,15 @@
+# Set any rules.mk overrides for your specific keymap here.
+# See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = no       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no        # Console for debug
+COMMAND_ENABLE = no        # Commands for debug and configuration
+NKRO_ENABLE = no           # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+RGB_MATRIX_ENABLE = yes
+MOUSE_SHARED_EP = no
+AUDIO_ENABLE = no
+ORYX_ENABLE = no
+SRC=matrix.c
